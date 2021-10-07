40d9e3d822051935a8821e550464bcf455f84595
Nikolas Laus <bchik3@gmail.com>
2021-10-07 13:11:32 +0300

    Add my exercise with filtering.

--- cats.php
+++ cats.php
@@ -7,7 +7,6 @@ $sorted_cats = array();
 foreach($cats as $cat) {
     array_push($sorted_cats, $cat);
 }
-$cats = $sorted_cats;
 
 // Compare cats by year of birth
 function comparator($cat1, $cat2) {
@@ -17,11 +16,16 @@ function comparator($cat1, $cat2) {
 // Sort cats
 usort($sorted_cats, 'comparator');
 
-function filter_condition($cat) {
+function filter_birthyear_condition($cat) {
     return $cat->birthyear == 2010;
 }
 
-$yeartwentyten_cats = array_filter($cats, 'filter_condition');
+function filter_afterbirthyear_string_length($cat) {
+    return $cat->birthyear > 2015 && strlen($cat -> name) < 6 && substr($cat -> name, 0, 1) == 'N';
+}
+
+$yeartwentyten_cats = array_filter($sorted_cats, 'filter_birthyear_condition');
+$myfilter_cats = array_filter($sorted_cats, 'filter_afterbirthyear_string_length');
 
 function print_cats($cats) {
     foreach($cats as $cat) {
@@ -51,9 +55,23 @@ function print_cats($cats) {
 </table>
 <hr>
 <table>
+    <th colspan="2">Sorted cats</th>
+    <?php
+    print_cats($sorted_cats);
+    ?>
+</table>
+<hr>
+<table>
     <th colspan="2">Cats, born in year 2010</th>
     <?php
     print_cats($yeartwentyten_cats);
+    ?>
+</table>
+<hr>
+<table>
+    <th colspan="2">Cats, born after year 2016 with less than 6 characters in the name, also N should be first character in the name.</th>
+    <?php
+    print_cats($myfilter_cats);
     ?>
 </table>
 <hr>
